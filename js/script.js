14b354b198618c98da454fee6f17b5c3d646cb31
Bogdan Langhe <bogdanlanghe@gmail.com>
2024-01-10 12:29:48 +0200

    Add 'thank-you' and 'thank-you__message' CSS classes
    The new styles for 'thank-you' and 'thank-you__message' have been added to 'main.scss' and '_typography.scss', respectively. Changes in 'main.scss' cascaded down to 'main.css' and 'main.css.map'. Improvements have also been made in 'script.js' to control the display of these new styles based on user action. Additionally, the HTML structure was updated to incorporate these classes.

--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@ const iconRating = document.querySelector(".icon_rating");
 const afterRating = document.querySelector(".after_rating");
 const selectionMade = document.querySelector(".selection-made");
 const form = document.querySelector(".form");
+const thankYouHeader = document.querySelector(".thank-you");
+const thankYouMessage = document.querySelector(".thank-you__message");
 
 submitBtn.addEventListener("click", () => {
   console.log(getRatingMessage());
-  iconRating.classList.add('d-none'); // Added code to add class 'd-none' to the iconRating element
-  afterRating.classList.remove('d-none');
+  iconRating.classList.add("d-none"); // Added code to add class 'd-none' to the iconRating element
+  afterRating.classList.remove("d-none");
   form.classList.add("d-none");
+  selectionMade.classList.remove("d-none");
   selectionMade.textContent = getRatingMessage();
-
+  thankYouHeader.classList.remove("d-none");
+  thankYouMessage.classList.remove("d-none");
 });
 
 function getCheckedRatingElementValue(elements) {
